c59313dd57c008da57ee317282cf0f4d3aa8ced3
MarcBaldi <marc.baldi@hotmail.de>
2020-05-16 13:54:11 +0200

    getUserMediaTry

--- src/main.js
+++ src/main.js
@@ -1,9 +1,68 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+// import 'vue-cam'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+
+// The width and height of the captured photo. We will set the
+// width to the value defined here, but the height will be
+// calculated based on the aspect ratio of the input stream.
+
+var width = 320 // We will scale the photo width to this
+var height = 0 // This will be computed based on the input stream
+
+// |streaming| indicates whether or not we're currently streaming
+// video from the camera. Obviously, we start at false.
+
+var streaming = false
+
+// The various HTML elements we need to configure or control. These
+// will be set by the startup() function.
+
+var video = null
+var canvas = null
+
+function startup () {
+  video = document.getElementById('video')
+  canvas = document.getElementById('canvas')
+
+  navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: false
+  })
+    .then(function (stream) {
+      video.srcObject = stream
+      video.play()
+    })
+    .catch(function (err) {
+      console.log('An error occurred: ' + err)
+    })
+
+  video.addEventListener('canplay', function (ev) {
+    if (!streaming) {
+      height = video.videoHeight / (video.videoWidth / width)
+
+      // Firefox currently has a bug where the height can't be read from
+      // the video, so we will make assumptions if this happens.
+
+      if (isNaN(height)) {
+        height = width / (4 / 3)
+      }
+
+      video.setAttribute('width', width)
+      video.setAttribute('height', height)
+      canvas.setAttribute('width', width)
+      canvas.setAttribute('height', height)
+      streaming = true
+    }
+  }, false)
+}
+
+// Set up our event listener to run the startup process
+// once loading is complete.
+window.addEventListener('load', startup, false)
