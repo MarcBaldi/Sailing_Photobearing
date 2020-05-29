968ae09ea6d46ec1ecf54a68eee5e483d58c092b
MarcBaldi <marc.baldi@hotmail.de>
2020-05-29 19:32:07 +0200

    access to SeaMarks is working

--- src/components/SeaSigns.vue
+++ src/components/SeaSigns.vue
@@ -1,14 +1,39 @@
 <template>
-    <div>
-      <p>DEBUG:</p>
-    </div>
+  <div id="app">
+    <h1>Nearby Nodes</h1>
+    {{ info }}
+    <p>DEBUG:</p>
+  </div>
 </template>
 
+// This component will show SeaMarks nodes near you.
+
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'SeaSigns'
+  name: 'SeaSigns',
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+      .get('http://overpass-api.de/api/interpreter?data=area[name="Bodensee"];nwr["seamark:type"~"^light"](area);out center;')
+      .then(response => (this.info = response.data))
+      .catch(error => console.log(error))
+  },
+  filters: {
+    currencydecimal (value) {
+      return value.toFixed(2)
+    }
+  }
 }
 </script>
+
+//  area[name="Bodensee"];nwr["seamark:type"~"^light"](area);out center;
+// overpass-api.de/api/
 
 <style scoped>
 
