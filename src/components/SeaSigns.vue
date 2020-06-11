30bd2fcaf806136c153ed6dc0ff00724d8b204c8
MarcBaldi <marc.baldi@hotmail.de>
2020-06-11 13:35:15 +0200

    added Geolib to calculate bearing of two lat-lon positions

--- src/components/SeaSigns.vue
+++ src/components/SeaSigns.vue
@@ -8,6 +8,7 @@
       id: {{ position.id }}
       lat: <span class="lighten">{{ position.lat }}</span>
       lon: <span class="lighten">{{ position.lon }}</span>
+      Dir: <span class="lighten">{{ withDirection(position.lat, position.lon) }}</span>
     </div>
     <!--<p>DEBUG: {{ info }}</p> -->
   </div>
@@ -15,8 +16,12 @@
 
 // This component will show SeaMarks nodes near you.
 
+// middle of Bodensee 47.603550, 9.424400
+// geolib.getRhumbLineBearing({ latitude: 52.518611, longitude: 13.408056 },{ latitude: 51.519475, longitude: 7.46694444 });
+
 <script>
 import axios from 'axios'
+import { getRhumbLineBearing } from 'geolib'
 
 export default {
   name: 'SeaSigns',
@@ -26,10 +31,17 @@ export default {
     }
   },
   mounted () {
+    // TODO: parameterize query
     axios
-      .get('http://overpass-api.de/api/interpreter?data=[out:json][timeout:60];area[name="Konstanz"];nwr["seamark:type"~"^light"](area);out center;')
+      .get('http://overpass-api.de/api/interpreter?data=[out:json][timeout:60];area[name="Bodensee"];nwr["seamark:type"~"^light"](area);out center;')
       .then(response => (this.info = response.data.elements))
       .catch(error => console.log(error))
+  },
+  methods: {
+    withDirection: function (lat, lon) {
+      // TODO: position of the user
+      return getRhumbLineBearing({ latitude: 47.603550, longitude: 9.424400 }, { lat, lon })
+    }
   }
 }
 </script>
