c80b2c82fd0589b5001ceeceddef00b82650e68f
MarcBaldi <marc.baldi@hotmail.de>
2020-06-13 22:08:49 +0200

    made CameraStream more vue friendly

--- src/components/CameraStream.vue
+++ src/components/CameraStream.vue
@@ -1,79 +1,72 @@
 <template>
   <div class="camera">
-    <video id="video">Video stream not available.</video>
+    <video id="video" ref="video" v-on:canplay="startStream">Your browser does not support the video tag.</video>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'CameraStream'
-}
-
-// The width and height of the captured photo. We will set the
-// width to the value defined here, but the height will be
-// calculated based on the aspect ratio of the input stream.
-
-var width = 412 // Samsung Galaxy s10+
-// var width = 320 // We will scale the photo width to this
-var height = 0 // This will be computed based on the input stream
-
-// |streaming| indicates whether or not we're currently streaming
-// video from the camera. Obviously, we start at false.
-
-var streaming = false
-
-// The various HTML elements we need to configure or control. These
-// will be set by the startup() function.
-
-var video = null
-
-function startup () {
-  video = document.getElementById('video')
-
-  navigator.mediaDevices.getUserMedia({
-    audio: false,
-    video: {
-      facingMode: 'environment'
+  name: 'CameraStream',
+  data () {
+    return {
+      width: 412, // Samsung Galaxy s10+
+      height: 869, // Samsung Galaxy s10+
+      streaming: false,
+      video: null
     }
-  })
-    .then(function (stream) {
-      video.srcObject = stream
-      video.play()
-    })
-    .catch(function (err) {
-      console.log('An error occurred: ' + err)
-    })
+  },
+  methods: {
+    startUp () {
+      // TODO: set width and height dynamically
 
-  video.addEventListener('canplay', function (ev) {
-    if (!streaming) {
-      height = video.videoHeight / (video.videoWidth / width)
+      navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: {
+          facingMode: 'environment'
+        }
+      })
+        .then(function (stream) {
+          this.video.srcObject = stream
+          this.video.play()
+        }.bind(this))
+        .catch(function (err) {
+          if (err === 'NotAllowedError') {
+            console.log('Maybe not in secure context. Error: ' + err)
+          } else {
+            console.log('An error occurred: ' + err)
+          }
+        })
+    },
+    startStream () {
+      if (!this.streaming) {
+        /* this.height = this.video.videoHeight / (this.video.videoWidth / this.width)
 
-      // Firefox currently has a bug where the height can't be read from
-      // the video, so we will make assumptions if this happens.
+        // Firefox currently has a bug where the height can't be read from
+        // the video, so we will make assumptions if this happens.
 
-      if (isNaN(height)) {
-        height = width / (4 / 3)
+        if (isNaN(this.height)) {
+          this.height = this.width / (4 / 3)
+          console.log('Using Firefox? Assuming 4/3 Ratio')
+        } */
+
+        this.video.setAttribute('width', this.width)
+        this.video.setAttribute('height', this.height)
+        this.streaming = true
       }
-
-      video.setAttribute('width', width)
-      video.setAttribute('height', height)
-      streaming = true
     }
-  }, false)
+  },
+  mounted () {
+    this.video = document.getElementById('video')
+    this.startUp()
+  }
 }
-
-// Set up our event listener to run the startup process
-// once loading is complete.
-window.addEventListener('load', startup, false)
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .camera {
-  z-index: 2;
   background-color: lightgray;
 }
 #video {
-  z-index: 2;
 }
 </style>
